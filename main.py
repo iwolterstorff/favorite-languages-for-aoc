7fa659df6574d776b3bfb67ef029bb46557178fc
Ian Wolterstorff <wolterstorff.i@northeastern.edu>
2021-12-07 00:19:36 -0500

    Remove dumb instances of tight coupling

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import collections
 import datetime
 import json
 import os
+import sys
 import time
 
 import dotenv
 import github
 import github.PaginatedList
 
-from typing import Any, DefaultDict, Dict, List, Set
+from typing import Any, DefaultDict, Dict, List, Set, TextIO
 
 search_terms: List[str] = [
     "adventofcode21",
@@ -28,15 +29,13 @@ dotenv.load_dotenv()
 # Put your access token in a .env file
 github_api = github.Github(os.getenv("GITHUB_ACCESS_TOKEN"), per_page=100)
 
-def combine_search_term_results(search_terms: List[str], save_dict_to_file: bool = False) -> Dict[str, int]:
+def combine_search_term_results(search_terms: List[str]) -> Dict[str, int]:
     """
     Querys Github's API, searching for repos that match a list of search terms, and then creates a dictionary
     mapping the names of programming languages to their prevalence in the results.
 
     Args:
         search_terms: A list of search terms to use to find repositories.
-        save_dict_to_file: If True, will save the dictionary output to a .json file in the same directory.
-            Defaults to False.
 
     Returns:
         A dictionary mapping the string names of programming language to their int prevalence in the search results.
@@ -49,8 +48,6 @@ def combine_search_term_results(search_terms: List[str], save_dict_to_file: bool
                 result_dict[repo.language] += 1
         except github.RateLimitExceededException:
             time.sleep(60) # Gotta wait to appease the almighty rate limiter ¯\_(ツ)_/¯
-    if save_dict_to_file:
-        save_to_file(result_dict)
     return result_dict
 
 def collect_and_rank_results(prevalence_dict: Dict[str, int]) -> List[str]:
@@ -61,10 +58,11 @@ def save_to_file(dict_to_save: Dict[Any, Any]) -> None:
     with open(f"github-results-{file_stamp}.json", mode="w") as f:
         json.dump(dict_to_save, f, indent=4)
 
-def main() -> None:
-    results = combine_search_term_results(search_terms, save_dict_to_file=True)
+def main(out_stream: TextIO) -> None:
+    results = combine_search_term_results(search_terms)
+    save_to_file(results) # TODO: Make this configurable by command-line args
     for lang in collect_and_rank_results(results):
-        print(lang)
+        out_stream.write(lang)
 
 if __name__ == "__main__":
-    main()
+    main(sys.stdout)
